b1caab26bdf7591a740db4143505065c12e8c9c4
Antonella <antonella.b95@hotmail.com>
2023-08-12 22:56:15 -0300

    estilos de página nosotros

---
+++ src/pages/Nosotros/NosotrosStyles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const AboutContainer = styled.section``;
+
+export const AboutImageContainer = styled.div``;
+
+export const AboutTextContainer = styled.div``;
+
+
+export const PaymentMethodContainer = styled.section``;
+
+export const PaymentMethodCardContainer = styled.div``;
+
+export const PaymentMethodCards = styled.div``;
+
+export const PaymentMethodCardInfo = styled.div``;
+
+export const PaymentMethodText = styled.div``;
+
+
+export const SocialMediaContainer = styled.section``;
+
+export const SocialMediaLinksContainer = styled.div``;
+
+export const WhatsappContainer = styled.div``;
+
