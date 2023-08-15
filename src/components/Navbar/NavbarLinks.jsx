e9108627bf5150e01d1e84f0704fa776ba2f443e
Antonella <antonella.b95@hotmail.com>
2023-08-15 08:06:23 -0300

    links del navbar

--- src/components/Navbar/NavbarLinks.jsx
+++ src/components/Navbar/NavbarLinks.jsx
@@ -1,13 +1,14 @@
 import { motion } from "framer-motion";
 import { LinkContainer, LinkStyled } from "./NavbarLinksStyles";
+import { links } from "./LinksData/LinksData";
 
-export const NavbarLinks = ({link}) => {
+export const NavbarLinks = ({title}) => {
     return (
         <LinkStyled>
         <motion.div whileTap={{scale: 0.90}}> 
           <a href="/#">
              <LinkContainer> 
-                {link}
+                {title}
              </LinkContainer>
           </a>
         </motion.div> 
