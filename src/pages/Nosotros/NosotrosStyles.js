42fdef2b3d0bc57ac56e8106f88a25f22419d479
Antonella <antonella.b95@hotmail.com>
2023-08-18 23:34:57 -0300

    estilos de páginas

--- src/pages/Nosotros/NosotrosStyles.js
+++ src/pages/Nosotros/NosotrosStyles.js
@@ -1,26 +1,134 @@
 import styled from "styled-components";
 
-export const AboutContainer = styled.section``;
+export const AboutContainer = styled.section`
+    max-width: 1200px;
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+    flex-wrap: wrap;
+    margin-top: 7rem;
 
-export const AboutImageContainer = styled.div``;
+    img {
+        width: 80%;
+        align-items: center;
+    }
+`;
 
-export const AboutTextContainer = styled.div``;
+export const AboutTextContainer = styled.div`
+    h1 {
+        font-size: 4rem;
+        font-family: 'Pathway Gothic One', sans-serif;
+        font-weight: 700;
+        letter-spacing: 3px;
+        color: #7e9dd4;
+        margin: 10px 0;
+    }
+
+    p {
+        font-size: 1.5rem;
+        font-family: 'Pathway Gothic One', sans-serif;
+        letter-spacing: 0.5px;
+        margin: 10px 0;
+        margin-bottom: 1.5rem;
+        color: #333;
+    }
+
+    @media (max-width: 1000px) {
+            width: 80%;
+            align-items: center;
+            text-align: center;
+        }
+`;
 
 
-export const PaymentMethodContainer = styled.section``;
+export const PaymentMethodContainer = styled.section`
+    display: flex;
+	flex-direction: column;
+    align-items: center;
+	max-width: 1200px;
+	width: 100%;
+	margin: 5rem 1rem;
+    h2 {
+        color: #7e9dd4;
+        font-family: 'Pathway Gothic One', sans-serif;
+        font-weight: 700;
+        letter-spacing: 3px;
+        text-transform: uppercase;
+        font-size: 3.1rem;
+    }
+	@media (max-width: 768px) {
+		padding-top: 30px;
+		text-align: center;
+	}
 
-export const PaymentMethodCardContainer = styled.div``;
+`;
 
-export const PaymentMethodCards = styled.div``;
+export const PaymentMethodCardContainer = styled.div`
+    display: flex;
+	flex-wrap: wrap;
+	justify-content: space-around;
+	align-items: center;
+	gap: 40px;
+`;
 
-export const PaymentMethodCardInfo = styled.div``;
+export const PaymentMethodCards = styled.div`
+    display: flex;
+	justify-content: center;
+	align-items: center;
+    background: #b4c8ea;
+    width: 270px;
+    border-radius: 1rem;
+    padding: 1rem;
 
-export const PaymentMethodText = styled.div``;
+    h3 {
+       color: #fff;
+       font-weight: 600;
+       font-family: 'Nanum Gothic Coding', sans-serif;
+       text-transform: uppercase;
+       letter-spacing: 1px;
+    }
+`;
+
+export const PaymentMethodText = styled.p`
+    font-size: 1.5rem;
+    font-family: 'Pathway Gothic One', sans-serif;
+    letter-spacing: 0.5px;
+    margin: 10px 0;
+    margin-bottom: 1.5rem;
+    color: #333;
+`;
 
 
-export const SocialMediaContainer = styled.section``;
+export const SocialMediaContainer = styled.section`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-wrap: wrap;
+	max-width: 1200px;
+	width: 100%;
+	margin: 5rem 1rem;
+`;
 
-export const SocialMediaLinksContainer = styled.div``;
+export const SocialMediaLinksContainer = styled.div`
+    h2 {
+        color: #7e9dd4;
+        font-family: 'Pathway Gothic One', sans-serif;
+        font-weight: 700;
+        letter-spacing: 3px;
+        text-transform: uppercase;
+        font-size: 2rem;
+    }
 
-export const WhatsappContainer = styled.div``;
+    p {
+        font-size: 1.5rem;
+        font-family: 'Pathway Gothic One', sans-serif;
+        letter-spacing: 0.5px;
+        margin: 10px 0;
+        margin-bottom: 1.5rem;
+        color: #333;
+    }
+`;
+
+export const WhatsappContainer = styled(SocialMediaLinksContainer)``;
+
 
