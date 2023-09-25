ac058486087110c0ae23732f871f42d99f163840
Antonella <antonella.b95@hotmail.com>
2023-09-24 23:25:22 -0300

    listas canciones

---
+++ src/data/ListasCanciones.js
@@ -0,0 +1,15 @@
+export const songList = [
+    {
+    id: 1,
+    songs: ["F*ck My Life", "손오공 (Super)", "Fire (Hip-Hop Team)", "I Don't Understand But I Luv U (Performance Team)", "먼지 (Dust) (Vocal Team)", "April shower"],
+    },
+    {
+    id: 2,
+    songs: ["놀고 놀아 (Circles)", "_WORLD", "Fallin' Flower (Korean Ver.)", "Cheers (SVT LEADERS)", "Darl+ing", "HOT", "DON QUIXOTE", "March", "Domino", "Shadow", "노래해 (Bout You)", "IF you leave me", "Ash"],
+    },
+    {
+    id: 3, 
+    songs: ["Darl+ing", "HOT", "DON QUIXOTE", "March", "Domino", "Shadow", "노래해 (Bout You)", "IF you leave me", "Ash"],
+    },
+    ];
+    
