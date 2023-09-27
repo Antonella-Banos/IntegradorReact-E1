8bc1b59e2db9d34ec7460326c09a5cf7ae02023b
Antonella <antonella.b95@hotmail.com>
2023-09-27 08:06:57 -0300

    modals styles

--- src/components/Modals/useStyles.jsx
+++ src/components/Modals/useStyles.jsx
@@ -4,19 +4,27 @@ export const useStyles = makeStyles((theme) => ({
     dialogContent: {
       padding: theme.spacing(2),
       border: '2px solid #ef94bd',
-      width: '300px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center', 
+
+      '@media (max-width: 900px)': {
+        width: '500px',
+      },
     },
     dialogContentText: {
       color: '#ef94bd',
       fontSize: '1.2rem',
       fontFamily: 'Pathway Gothic One',
       textAlign: 'center',
+      flexWrap: 'wrap', 
     },
     button: {
       backgroundColor: '#ef94bd',
       color: '#fff',
-      '&:hover': {
-        backgroundColor: 'linear-gradient(100deg, #92a8d1, #f7cac9)',
+      transition: 'opacity 0.3s',
+      '&:active': {
+        opacity: 0.6,  
       },
     },
   }));
